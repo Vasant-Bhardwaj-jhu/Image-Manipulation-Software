51730fe8d879da3ba35efa6fd314a0666cd9e1fe
Vasant-Bhardwaj-jhu <vbhardw2@jhu.edu>
2020-03-17 14:57:30 -0400

    finished read_ppm: added main to file for checking only

--- ppm_io.c
+++ ppm_io.c
@@ -5,6 +5,7 @@
 // Starter code for midterm project - feel free to edit/add to this file
 
 #include <assert.h>
+#include <stdlib.h>
 #include "ppm_io.h"
 
 
@@ -16,10 +17,38 @@
 Image * read_ppm(FILE *fp) {
 
   // check that fp is not NULL
-  assert(fp); 
-
-  return NULL;  //TO DO: replace this stub
+  assert(fp);
+  Image *im = malloc(sizeof(Image));
   
+  fseek (fp, 0, SEEK_END);
+  long lsize = ftell(fp);
+  rewind(fp);
+  char buffer[2];
+  int color;
+  fscanf(fp, "%s %d %d %d", buffer, &im->rows, &im->cols, &color);
+  if(buffer[0] != 'P' || buffer[1] != '6'){
+    printf("incorrect file format");
+    return NULL;
+  }
+  if(color != 255){
+    printf("wrong hue value");
+    return NULL;
+  }
+   Pixel *pix = malloc(sizeof(Pixel) * im->rows * im->cols);
+   if (!pix) {
+    free(im);
+    return NULL;
+  }
+   im->data = pix;
+  for (int r = 0; r < im->rows; r++) {
+    for (int c = 0; c < im->cols; c++) {
+      fread(&im->data[(r*im->cols) + c], sizeof(Pixel), lsize , fp);
+    }
+  }
+
+  
+  return im;  //TO DO: replace this stub
+  free(im);
 }
 
 
@@ -45,5 +74,11 @@ int write_ppm(FILE *fp, const Image *im) {
   }
 
   return num_pixels_written;
+}
+
+int main() {
+    printf("running");
+    FILE * in = fopen("trees.ppm", "rb");
+    Image *i = read_ppm(in); 
 }
 
