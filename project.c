b258e1728bcee73c4a0efd8c6445b2867ff95dec
tsikorksi <tsikors2@jhu.edu>
2020-03-17 16:11:57 +0100

    Added core files to structure

---
+++ project.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+
+ // Reads file using ppm_io, sends to correct function in imageManip.c
+ // Handles very basic read errors/command line errors
+int main( int argc, char *argv[] ) {
+    return 0;
+}
