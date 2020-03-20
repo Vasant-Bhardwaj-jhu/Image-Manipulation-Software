0afc21406032a58de03c2fdc4a26926f823926c6
tsikorksi <tsikors2@jh.edu>
2020-03-20 20:32:37 +0100

    Input should now work for all functions

--- project.c
+++ project.c
@@ -7,47 +7,71 @@
 
 // Reads file using ppm_io, sends to correct function in imageManip.c
 // Handles very basic read errors/command line errors
- int select_function(int argc, char *argv[]){
+int select_function(int argc, char *argv[]){
 
-     // Missing one of the required arguments
-     if (argc < 4) {
-         kill(1);
-     }
+    // Missing one of the required arguments
+    if (argc < 4) {
+        kill(1);
+    }
 
-     FILE * in = fopen(argv[1], "rb");
-     FILE * out = fopen(argv[2], "rb");
+    FILE * in = fopen(argv[1], "rb");
+    FILE * out = fopen(argv[2], "rb");
 
-     if (in == NULL) {
-         kill(2);
-     }
-     if (out == NULL) {
-         kill(7);
-     }
+    if (in == NULL) {
+        kill(2);
+    }
+    if (out == NULL) {
+        kill(7);
+    }
 
-     Image *i = read_ppm(in);
+    Image *i = read_ppm(in);
 
-     if (i == NULL) {
-         kill(3);
-     }
+    if (i == NULL) {
+        kill(3);
+    }
 
-     // Function Execution Begins
-     char choice[12];
-     strcpy(choice, argv[3]);
+    // Function Execution Begins
+    char choice[12];
+    strcpy(choice, argv[3]);
 
-     if (strcmp(choice, "exposure") == 0) {
-         float n = strtof(argv[4], NULL);
-         exposure(in, out, n);
-     }
-     else if (strcmp(choice, "aBlending") == 0) {
-         int n = atoi(argv[5]);
-//        FILE *in2 = fopen(argv[3], "rb");
-//        if (in2 == NULL) {
-//            kill(2);
-//        }
-         exposure(in, out, n);
-     }
-     return 0;
- }
+    if (strcmp(choice, "exposure") == 0) {
+        double n = atof(argv[4]);
+        exposure(in, out, n);
+    }
+    else if (strcmp(choice, "aBlending") == 0) {
+        int n = atoi(argv[5]);
+        FILE *in2 = fopen(argv[3], "rb");
+        if (in2 == NULL) {
+            kill(2);
+        }
+        aBlending(in, in2, out, n);
+    }
+    else if (strcmp(choice, "zoom_in") == 0) {
+        float n = strtof(argv[4], NULL);
+        zoom_in(in, out, n);
+    }
+    else if (strcmp(choice, "zoom_out") == 0) {
+        float n = strtof(argv[4], NULL);
+        zoom_in(in, out, n);
+    }
+    else if (strcmp(choice, "pointilism") == 0) {
+        pointilism(in, out);
+    }
+    else if (strcmp(choice, "swirl") == 0) {
+        float x = strtof(argv[4], NULL);
+        float y = strtof(argv[5], NULL);
+        float strength = strtof(argv[6], NULL);
+        swirl(in, out, x, y, strength);
+    }
+    else if (strcmp(choice, "blur") == 0) {
+        float sigma = strtof(argv[4], NULL);
+        blur(in, out, sigma);
+    }
+    else {
+        kill(4);
+    }
+    return 0;
+}
 
 
 int main( int argc, char *argv[] ) {
